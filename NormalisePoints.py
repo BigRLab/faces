2a4b3e80137d7f8c0a0044c5ba675a53f001cffe
George Lin <zhujin.lin@gmail.com>
2017-10-14 20:02:06 +0100

    Convert to spaces

--- NormalisePoints.py
+++ NormalisePoints.py
@@ -1,147 +1,161 @@
 from pymongo import MongoClient
 import math
 
-#-------------------------------
+
+# -------------------------------
 def main():
-	# Init MongoDB connection
-	client = MongoClient('mongodb://fetcher:fetcherpass@ds119395.mlab.com:19395/faces_db')
-	db = client.faces_db
-	raw_collection = db.raw_image_points
-	normalised_collection = db.normalised_collection
+    # Init MongoDB connection
+    client = MongoClient('mongodb://fetcher:fetcherpass@ds119395.mlab.com:19395/faces_db')
+    db = client.faces_db
+    raw_collection = db.raw_image_points
+    normalised_collection = db.normalised_collection
 
-	# Go through all the raw images
-	for image_json in raw_collection.find():
-		processImage(image_json, normalised_collection, 40, 40)
+    # Go through all the raw images
+    for image_json in raw_collection.find():
+        processImage(image_json, normalised_collection, 40, 40)
 
-	return
+    return
 
-#-------------------------------
+
+# -------------------------------
 def processImage(image_json, normalised_collection, normalised_width, normalised_height):
-	# In case we have already done this one
-	cursor = normalised_collection.find({"file_name": image_json["file_name"]})
-	if cursor.count() > 0:
-		print( "Image already processed - skipping {}".format( image_json["file_name"] ) )
-		return
+    # In case we have already done this one
+    cursor = normalised_collection.find({"file_name": image_json["file_name"]})
+    if cursor.count() > 0:
+        print("Image already processed - skipping {}".format(image_json["file_name"]))
+        return
 
-	#for debugging Romano_Prodi_0005 
-	if image_json["file_name"] != "Aaron_Eckhart_0001.jpg":
-		return
+        # for debugging Romano_Prodi_0005
+    if image_json["file_name"] != "Aaron_Eckhart_0001.jpg":
+        return
 
-	print( image_json )
-	
-	#if len( image_json["faces"][0]["landmarks"][0][0] ) > 1:
-	#	return
+    print(image_json)
 
-	# flatten and return all points, we dont care about which part of the face they relate to for now
-	face_landmarks = fetch_all_points( image_json["faces"][0]["landmarks"] )
+    # if len( image_json["faces"][0]["landmarks"][0][0] ) > 1:
+    #	return
 
-	print( "face_landmarks: {}".format( face_landmarks ) )
+    # flatten and return all points, we dont care about which part of the face they relate to for now
+    face_landmarks = fetch_all_points(image_json["faces"][0]["landmarks"])
 
-	# boundary of the face within the photo
-	face_boundary = image_json["faces"][0]["bounding_box"]
+    print("face_landmarks: {}".format(face_landmarks))
 
-	# remove offset from top left corner
-	top_left = move_image_top_left( face_landmarks, face_boundary )
+    # boundary of the face within the photo
+    face_boundary = image_json["faces"][0]["bounding_box"]
 
-	print( "face_boundary: {}".format( face_boundary ) )
-	print( "top_left: {}".format( top_left ) )
-	print()
-	normalised_landmarks = normalise_landmarks( top_left, face_boundary, normalised_width, normalised_height )
+    # remove offset from top left corner
+    top_left = move_image_top_left(face_landmarks, face_boundary)
 
-	# Convert to binary true false array to represent each point
-	binary_landmarks = convert_binary_landmarks( normalised_landmarks, normalised_width, normalised_height )
+    print("face_boundary: {}".format(face_boundary))
+    print("top_left: {}".format(top_left))
 
-	for i in range( len( binary_landmarks ) ):
-		if binary_landmarks[ i ] == 1:
-			print( "X", end="" )
-		else:
-			print( ".", end="" )
+    normalised_landmarks = normalise_landmarks(top_left, face_boundary, normalised_width, normalised_height)
 
-		if (i + 1) % normalised_width == 0:
-			print( "" )
-		
-	json = {"file_name": image_json["file_name"],"normalised_landmarks":normalised_landmarks,"binary_landmarks":binary_landmarks,"male_female":image_json["male_female"]}
+    # Convert to binary true false array to represent each point
+    binary_landmarks = convert_binary_landmarks(normalised_landmarks, normalised_width, normalised_height)
 
-	#result = normalised_collection.insert_one(json)
-	#print( "MongoDB ID: {}".format( result.inserted_id ) )
-	
-	print( json )
+    print_ascii_face(binary_landmarks, normalised_width)
 
-	return
+    json = {"file_name": image_json["file_name"], "normalised_landmarks": normalised_landmarks,
+            "binary_landmarks": binary_landmarks, "male_female": image_json["male_female"]}
 
-#-------------------------------
-def move_image_top_left( face_landmarks, face_boundary ):
-	centered_marks = []
+    # result = normalised_collection.insert_one(json)
+    # print( "MongoDB ID: {}".format( result.inserted_id ) )
 
-	for point in face_landmarks:
-		x = math.floor( point[0] - face_boundary[0] )
-		y = math.floor( point[1] - face_boundary[1] )
-		centered_marks.append( [ x, y ] )
+    # print( json )
 
-	return centered_marks
+    return
 
-#-------------------------------
-def convert_binary_landmarks( normalised_landmarks, normalised_width, normalised_height ):
-	binary_array = [0] * normalised_width * normalised_height
+# -------------------------------
+def print_ascii_face(binary_landmarks, width):
 
-	print( "Size {}".format( len( binary_array ) ) )
+    for i in range(len(binary_landmarks)):
+        if binary_landmarks[i] == 1:
+            print("X", end="")
+        else:
+            print(".", end="")
 
-	for mark in normalised_landmarks:
-		#print( "mark[ 0 ] {}, mark[ 1 ] {}, index {}".format( mark[ 0 ], mark[ 1 ], mark[ 1 ] * normalised_width + mark[ 0 ] ) )
-		binary_array[ mark[ 1 ] * normalised_width + mark[ 0 ] ] = 1
+        if (i + 1) % width == 0:
+            print("")
 
-	return binary_array
+    return
 
-#-------------------------------
+# -------------------------------
+def move_image_top_left(face_landmarks, face_boundary):
+    centered_marks = []
+
+    for point in face_landmarks:
+        x = math.floor(point[0] - face_boundary[0])
+        y = math.floor(point[1] - face_boundary[1])
+        centered_marks.append([x, y])
+
+    return centered_marks
+
+
+# -------------------------------
+def convert_binary_landmarks(normalised_landmarks, normalised_width, normalised_height):
+    binary_array = [0] * normalised_width * normalised_height
+
+    print("Size {}".format(len(binary_array)))
+
+    for mark in normalised_landmarks:
+        # print( "mark[ 0 ] {}, mark[ 1 ] {}, index {}".format( mark[ 0 ], mark[ 1 ], mark[ 1 ] * normalised_width + mark[ 0 ] ) )
+        binary_array[mark[1] * normalised_width + mark[0]] = 1
+
+    return binary_array
+
+
+# -------------------------------
 def normalise_landmarks(face_landmarks, face_boundary, normalised_width, normalised_height):
-	normalised_landmarks = []
+    normalised_landmarks = []
 
-	face_width = face_boundary[2] - face_boundary[0]
-	face_height = face_boundary[3] - face_boundary[1]
+    face_width = face_boundary[2] - face_boundary[0]
+    face_height = face_boundary[3] - face_boundary[1]
 
-	print( "face_width {}, face_height {}".format( face_width, face_height ) )
+    print("face_width {}, face_height {}".format(face_width, face_height))
 
-	width_ratio = normalised_width / face_width
-	height_ratio = normalised_height / face_height
+    width_ratio = normalised_width / face_width
+    height_ratio = normalised_height / face_height
 
-	print( "width_ratio {}, height_ratio {}".format( width_ratio, height_ratio ) )
+    print("width_ratio {}, height_ratio {}".format(width_ratio, height_ratio))
 
-	smaller_ratio = width_ratio
-	height_adjust = ( normalised_height - ( face_height * smaller_ratio ) ) / 2
-	width_adjust = 0
-	if height_ratio < width_ratio:
-		smaller_ratio = height_ratio
-		height_adjust = 0
-		width_adjust = ( normalised_width - ( face_width * smaller_ratio ) ) / 2
+    smaller_ratio = width_ratio
+    height_adjust = (normalised_height - (face_height * smaller_ratio)) / 2
+    width_adjust = 0
+    if height_ratio < width_ratio:
+        smaller_ratio = height_ratio
+        height_adjust = 0
+        width_adjust = (normalised_width - (face_width * smaller_ratio)) / 2
 
-	print( "smaller_ratio {}, width_adjust {}, height_adjust {}".format( smaller_ratio, width_adjust, height_adjust ) )
-	
-	for point in face_landmarks:
-		x = min( normalised_width - 1, math.ceil( point[0] * smaller_ratio + width_adjust ) )
-		y = min( normalised_height - 1, math.ceil( point[1] * smaller_ratio + height_adjust ) )
-		print( [x,y], end=" " )
-		normalised_landmarks.append([ x, y ])
+    print("smaller_ratio {}, width_adjust {}, height_adjust {}".format(smaller_ratio, width_adjust, height_adjust))
 
-	return normalised_landmarks
+    for point in face_landmarks:
+        x = min(normalised_width - 1, math.ceil(point[0] * smaller_ratio + width_adjust))
+        y = min(normalised_height - 1, math.ceil(point[1] * smaller_ratio + height_adjust))
+        print([x, y], end=" ")
+        normalised_landmarks.append([x, y])
 
-#-------------------------------
-def fetch_all_points( raw_landmarks ):
-	all_points = []
+    return normalised_landmarks
 
-	# flatten and return all points, we dont care about which part of the face they relate to for now
-	element = raw_landmarks[ 0 ][ 0 ]
-	all_points.extend(element[ "nose_bridge" ])
-	all_points.extend(element[ "left_eye" ])
-	all_points.extend(element[ "nose_tip" ])
-	all_points.extend(element[ "chin" ])
-	all_points.extend(element[ "right_eye" ])
-	all_points.extend(element[ "left_eyebrow" ])
-	all_points.extend(element[ "bottom_lip" ])
-	all_points.extend(element[ "right_eyebrow" ])
-	all_points.extend(element[ "top_lip" ])
 
-	return all_points
+# -------------------------------
+def fetch_all_points(raw_landmarks):
+    all_points = []
 
-#-------------------------------
+    # flatten and return all points, we dont care about which part of the face they relate to for now
+    element = raw_landmarks[0][0]
+    all_points.extend(element["nose_bridge"])
+    all_points.extend(element["left_eye"])
+    all_points.extend(element["nose_tip"])
+    all_points.extend(element["chin"])
+    all_points.extend(element["right_eye"])
+    all_points.extend(element["left_eyebrow"])
+    all_points.extend(element["bottom_lip"])
+    all_points.extend(element["right_eyebrow"])
+    all_points.extend(element["top_lip"])
+
+    return all_points
+
+
+# -------------------------------
 if __name__ == "__main__":
     main()
